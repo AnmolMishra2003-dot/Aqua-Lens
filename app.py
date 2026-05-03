9aff1ddad7994ff0bce8674749100c4b58ac1f3f
AnmolMishra2003-dot <itsanmolmishra2@gmail.com>
2026-05-03 21:41:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -354,4 +354,4 @@ if __name__ == '__main__':
 
     load_tflite_model()
     print("\n🚀 Server at http://localhost:5000\n")
-   app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
